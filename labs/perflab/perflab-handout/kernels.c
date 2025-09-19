2a32e5f4301c770d1ef2616add303612568d7651
chensai <chensaichochensai@foxmail.com>
2025-09-19 09:37:11 +0800

    format perflab/kernels.c

--- labs/perflab/perflab-handout/kernels.c
+++ labs/perflab/perflab-handout/kernels.c
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+
 #include "defs.h"
 
 /*
@@ -31,13 +32,12 @@ team_t team = {
  * naive_rotate - The naive baseline version of rotate
  */
 char naive_rotate_descr[] = "naive_rotate: Naive baseline implementation";
-void naive_rotate(int dim, pixel *src, pixel *dst)
-{
-    int i, j;
+void naive_rotate(int dim, pixel *src, pixel *dst) {
+  int i, j;
 
-    for (i = 0; i < dim; i++)
-        for (j = 0; j < dim; j++)
-            dst[RIDX(dim - 1 - j, i, dim)] = src[RIDX(i, j, dim)];
+  for (i = 0; i < dim; i++)
+    for (j = 0; j < dim; j++)
+      dst[RIDX(dim - 1 - j, i, dim)] = src[RIDX(i, j, dim)];
 }
 
 /*
@@ -45,28 +45,24 @@ void naive_rotate(int dim, pixel *src, pixel *dst)
  * IMPORTANT: This is the version you will be graded on
  */
 char rotate_descr[] = "rotate: Current working version";
-void rotate(int dim, pixel *src, pixel *dst)
-{
-    int i, j, k, b;
-    b = 8; // block size
+void rotate(int dim, pixel *src, pixel *dst) {
+  int i, j, k, b;
+  b = 8; // block size
 
-    for (i = 0; i < dim; i += b)
-    {
-        for (j = 0; j < dim; j += b)
-        {
-            for (k = 0; k < b; k++)
-            {
-                dst[RIDX(dim - 1 - j - 0, i + k, dim)] = src[RIDX(i + k, j + 0, dim)];
-                dst[RIDX(dim - 1 - j - 1, i + k, dim)] = src[RIDX(i + k, j + 1, dim)];
-                dst[RIDX(dim - 1 - j - 2, i + k, dim)] = src[RIDX(i + k, j + 2, dim)];
-                dst[RIDX(dim - 1 - j - 3, i + k, dim)] = src[RIDX(i + k, j + 3, dim)];
-                dst[RIDX(dim - 1 - j - 4, i + k, dim)] = src[RIDX(i + k, j + 4, dim)];
-                dst[RIDX(dim - 1 - j - 5, i + k, dim)] = src[RIDX(i + k, j + 5, dim)];
-                dst[RIDX(dim - 1 - j - 6, i + k, dim)] = src[RIDX(i + k, j + 6, dim)];
-                dst[RIDX(dim - 1 - j - 7, i + k, dim)] = src[RIDX(i + k, j + 7, dim)];
-            }
-        }
+  for (i = 0; i < dim; i += b) {
+    for (j = 0; j < dim; j += b) {
+      for (k = 0; k < b; k++) {
+        dst[RIDX(dim - 1 - j - 0, i + k, dim)] = src[RIDX(i + k, j + 0, dim)];
+        dst[RIDX(dim - 1 - j - 1, i + k, dim)] = src[RIDX(i + k, j + 1, dim)];
+        dst[RIDX(dim - 1 - j - 2, i + k, dim)] = src[RIDX(i + k, j + 2, dim)];
+        dst[RIDX(dim - 1 - j - 3, i + k, dim)] = src[RIDX(i + k, j + 3, dim)];
+        dst[RIDX(dim - 1 - j - 4, i + k, dim)] = src[RIDX(i + k, j + 4, dim)];
+        dst[RIDX(dim - 1 - j - 5, i + k, dim)] = src[RIDX(i + k, j + 5, dim)];
+        dst[RIDX(dim - 1 - j - 6, i + k, dim)] = src[RIDX(i + k, j + 6, dim)];
+        dst[RIDX(dim - 1 - j - 7, i + k, dim)] = src[RIDX(i + k, j + 7, dim)];
+      }
     }
+  }
 }
 
 /*********************************************************************
@@ -77,11 +73,10 @@ void rotate(int dim, pixel *src, pixel *dst)
  *     registered test function.
  *********************************************************************/
 
-void register_rotate_functions()
-{
-    add_rotate_function(&naive_rotate, naive_rotate_descr);
-    add_rotate_function(&rotate, rotate_descr);
-    /* ... Register additional test functions here */
+void register_rotate_functions() {
+  add_rotate_function(&naive_rotate, naive_rotate_descr);
+  add_rotate_function(&rotate, rotate_descr);
+  /* ... Register additional test functions here */
 }
 
 /***************
@@ -94,12 +89,11 @@ void register_rotate_functions()
  **************************************************************/
 
 /* A struct used to compute averaged pixel value */
-typedef struct
-{
-    int red;
-    int green;
-    int blue;
-    int num;
+typedef struct {
+  int red;
+  int green;
+  int blue;
+  int num;
 } pixel_sum;
 
 /* Compute min and max of two integers, respectively */
@@ -109,90 +103,124 @@ static int max(int a, int b) { return (a > b ? a : b); }
 /*
  * initialize_pixel_sum - Initializes all fields of sum to 0
  */
-static void initialize_pixel_sum(pixel_sum *sum)
-{
-    sum->red = sum->green = sum->blue = 0;
-    sum->num = 0;
-    return;
+static void initialize_pixel_sum(pixel_sum *sum) {
+  sum->red = sum->green = sum->blue = 0;
+  sum->num = 0;
+  return;
 }
 
 /*
  * accumulate_sum - Accumulates field values of p in corresponding
  * fields of sum
  */
-static void accumulate_sum(pixel_sum *sum, pixel p)
-{
-    sum->red += (int)p.red;
-    sum->green += (int)p.green;
-    sum->blue += (int)p.blue;
-    sum->num++;
-    return;
+static void accumulate_sum(pixel_sum *sum, pixel p) {
+  sum->red += (int)p.red;
+  sum->green += (int)p.green;
+  sum->blue += (int)p.blue;
+  sum->num++;
+  return;
 }
 
 /*
  * assign_sum_to_pixel - Computes averaged pixel value in current_pixel
  */
-static void assign_sum_to_pixel(pixel *current_pixel, pixel_sum sum)
-{
-    current_pixel->red = (unsigned short)(sum.red / sum.num);
-    current_pixel->green = (unsigned short)(sum.green / sum.num);
-    current_pixel->blue = (unsigned short)(sum.blue / sum.num);
-    return;
+static void assign_sum_to_pixel(pixel *current_pixel, pixel_sum sum) {
+  current_pixel->red = (unsigned short)(sum.red / sum.num);
+  current_pixel->green = (unsigned short)(sum.green / sum.num);
+  current_pixel->blue = (unsigned short)(sum.blue / sum.num);
+  return;
 }
 
 /*
  * avg - Returns averaged pixel value at (i,j)
  */
-static pixel avg(int dim, int i, int j, pixel *src)
-{
-    int ii, jj;
-    pixel_sum sum;
-    pixel current_pixel;
+static pixel avg(int dim, int i, int j, pixel *src) {
+  int ii, jj;
+  pixel_sum sum;
+  pixel current_pixel;
 
-    initialize_pixel_sum(&sum);
-    for (ii = max(i - 1, 0); ii <= min(i + 1, dim - 1); ii++)
-        for (jj = max(j - 1, 0); jj <= min(j + 1, dim - 1); jj++)
-            accumulate_sum(&sum, src[RIDX(ii, jj, dim)]);
+  initialize_pixel_sum(&sum);
+  for (ii = max(i - 1, 0); ii <= min(i + 1, dim - 1); ii++)
+    for (jj = max(j - 1, 0); jj <= min(j + 1, dim - 1); jj++)
+      accumulate_sum(&sum, src[RIDX(ii, jj, dim)]);
 
-    assign_sum_to_pixel(&current_pixel, sum);
-    return current_pixel;
+  assign_sum_to_pixel(&current_pixel, sum);
+  return current_pixel;
 }
 
-static void set_corner(int cc, pixel *src, pixel *dst, int a1, int a2, int a3)
-{
-    dst[cc].blue = (src[cc].blue + src[a1].blue + src[a2].blue + src[a3].blue) >> 2;
-    dst[cc].green = (src[cc].green + src[a1].green + src[a2].green + src[a3].green) >> 2;
-    dst[cc].red = (src[cc].red + src[a1].red + src[a2].red + src[a3].red) >> 2;
+static void set_corner(int cc, pixel *src, pixel *dst, int a1, int a2, int a3) {
+  dst[cc].blue =
+      (src[cc].blue + src[a1].blue + src[a2].blue + src[a3].blue) >> 2;
+  dst[cc].green =
+      (src[cc].green + src[a1].green + src[a2].green + src[a3].green) >> 2;
+  dst[cc].red = (src[cc].red + src[a1].red + src[a2].red + src[a3].red) >> 2;
 }
-static void set_top(int dim, pixel *src, pixel *dst, int j)
-{
-    dst[j].blue = (src[j].blue + src[j + dim].blue + src[j - 1].blue + src[j + 1].blue + src[j + dim - 1].blue + src[j + dim + 1].blue) / 6;
-    dst[j].green = (src[j].green + src[j + dim].green + src[j - 1].green + src[j + 1].green + src[j + dim - 1].green + src[j + dim + 1].green) / 6;
-    dst[j].red = (src[j].red + src[j + dim].red + src[j - 1].red + src[j + 1].red + src[j + dim - 1].red + src[j + dim + 1].red) / 6;
+static void set_top(int dim, pixel *src, pixel *dst, int j) {
+  dst[j].blue =
+      (src[j].blue + src[j + dim].blue + src[j - 1].blue + src[j + 1].blue +
+       src[j + dim - 1].blue + src[j + dim + 1].blue) /
+      6;
+  dst[j].green =
+      (src[j].green + src[j + dim].green + src[j - 1].green + src[j + 1].green +
+       src[j + dim - 1].green + src[j + dim + 1].green) /
+      6;
+  dst[j].red = (src[j].red + src[j + dim].red + src[j - 1].red +
+                src[j + 1].red + src[j + dim - 1].red + src[j + dim + 1].red) /
+               6;
 }
-static void set_bottom(int dim, pixel *src, pixel *dst, int j)
-{
-    dst[j].blue = (src[j].blue + src[j - dim].blue + src[j - 1].blue + src[j + 1].blue + src[j - dim - 1].blue + src[j - dim + 1].blue) / 6;
-    dst[j].green = (src[j].green + src[j - dim].green + src[j - 1].green + src[j + 1].green + src[j - dim - 1].green + src[j - dim + 1].green) / 6;
-    dst[j].red = (src[j].red + src[j - dim].red + src[j - 1].red + src[j + 1].red + src[j - dim - 1].red + src[j - dim + 1].red) / 6;
+static void set_bottom(int dim, pixel *src, pixel *dst, int j) {
+  dst[j].blue =
+      (src[j].blue + src[j - dim].blue + src[j - 1].blue + src[j + 1].blue +
+       src[j - dim - 1].blue + src[j - dim + 1].blue) /
+      6;
+  dst[j].green =
+      (src[j].green + src[j - dim].green + src[j - 1].green + src[j + 1].green +
+       src[j - dim - 1].green + src[j - dim + 1].green) /
+      6;
+  dst[j].red = (src[j].red + src[j - dim].red + src[j - 1].red +
+                src[j + 1].red + src[j - dim - 1].red + src[j - dim + 1].red) /
+               6;
 }
-static void set_left(int dim, pixel *src, pixel *dst, int i)
-{
-    dst[i].blue = (src[i].blue + src[i - dim].blue + src[i - dim + 1].blue + src[i + 1].blue + src[i + dim].blue + src[i + dim + 1].blue) / 6;
-    dst[i].green = (src[i].green + src[i - dim].green + src[i - dim + 1].green + src[i + 1].green + src[i + dim].green + src[i + dim + 1].green) / 6;
-    dst[i].red = (src[i].red + src[i - dim].red + src[i - dim + 1].red + src[i + 1].red + src[i + dim].red + src[i + dim + 1].red) / 6;
+static void set_left(int dim, pixel *src, pixel *dst, int i) {
+  dst[i].blue = (src[i].blue + src[i - dim].blue + src[i - dim + 1].blue +
+                 src[i + 1].blue + src[i + dim].blue + src[i + dim + 1].blue) /
+                6;
+  dst[i].green =
+      (src[i].green + src[i - dim].green + src[i - dim + 1].green +
+       src[i + 1].green + src[i + dim].green + src[i + dim + 1].green) /
+      6;
+  dst[i].red = (src[i].red + src[i - dim].red + src[i - dim + 1].red +
+                src[i + 1].red + src[i + dim].red + src[i + dim + 1].red) /
+               6;
 }
-static void set_right(int dim, pixel *src, pixel *dst, int i)
-{
-    dst[i].blue = (src[i].blue + src[i - dim].blue + src[i - dim - 1].blue + src[i - 1].blue + src[i + dim].blue + src[i + dim - 1].blue) / 6;
-    dst[i].green = (src[i].green + src[i - dim].green + src[i - dim - 1].green + src[i - 1].green + src[i + dim].green + src[i + dim - 1].green) / 6;
-    dst[i].red = (src[i].red + src[i - dim].red + src[i - dim - 1].red + src[i - 1].red + src[i + dim].red + src[i + dim - 1].red) / 6;
+static void set_right(int dim, pixel *src, pixel *dst, int i) {
+  dst[i].blue = (src[i].blue + src[i - dim].blue + src[i - dim - 1].blue +
+                 src[i - 1].blue + src[i + dim].blue + src[i + dim - 1].blue) /
+                6;
+  dst[i].green =
+      (src[i].green + src[i - dim].green + src[i - dim - 1].green +
+       src[i - 1].green + src[i + dim].green + src[i + dim - 1].green) /
+      6;
+  dst[i].red = (src[i].red + src[i - dim].red + src[i - dim - 1].red +
+                src[i - 1].red + src[i + dim].red + src[i + dim - 1].red) /
+               6;
 }
-static void set_in(int dim, pixel *src, pixel *dst, int k)
-{
-    dst[k].blue = (src[k].blue + src[k - 1].blue + src[k + 1].blue + src[k + dim - 1].blue + src[k + dim].blue + src[k + dim + 1].blue + src[k - dim - 1].blue + src[k - dim].blue + src[k - dim + 1].blue) / 9;
-    dst[k].green = (src[k].green + src[k - 1].green + src[k + 1].green + src[k + dim - 1].green + src[k + dim].green + src[k + dim + 1].green + src[k - dim - 1].green + src[k - dim].green + src[k - dim + 1].green) / 9;
-    dst[k].red = (src[k].red + src[k - 1].red + src[k + 1].red + src[k + dim - 1].red + src[k + dim].red + src[k + dim + 1].red + src[k - dim - 1].red + src[k - dim].red + src[k - dim + 1].red) / 9;
+static void set_in(int dim, pixel *src, pixel *dst, int k) {
+  dst[k].blue =
+      (src[k].blue + src[k - 1].blue + src[k + 1].blue + src[k + dim - 1].blue +
+       src[k + dim].blue + src[k + dim + 1].blue + src[k - dim - 1].blue +
+       src[k - dim].blue + src[k - dim + 1].blue) /
+      9;
+  dst[k].green =
+      (src[k].green + src[k - 1].green + src[k + 1].green +
+       src[k + dim - 1].green + src[k + dim].green + src[k + dim + 1].green +
+       src[k - dim - 1].green + src[k - dim].green + src[k - dim + 1].green) /
+      9;
+  dst[k].red =
+      (src[k].red + src[k - 1].red + src[k + 1].red + src[k + dim - 1].red +
+       src[k + dim].red + src[k + dim + 1].red + src[k - dim - 1].red +
+       src[k - dim].red + src[k - dim + 1].red) /
+      9;
 }
 
 /******************************************************
@@ -203,13 +231,12 @@ static void set_in(int dim, pixel *src, pixel *dst, int k)
  * naive_smooth - The naive baseline version of smooth
  */
 char naive_smooth_descr[] = "naive_smooth: Naive baseline implementation";
-void naive_smooth(int dim, pixel *src, pixel *dst)
-{
-    int i, j;
+void naive_smooth(int dim, pixel *src, pixel *dst) {
+  int i, j;
 
-    for (i = 0; i < dim; i++)
-        for (j = 0; j < dim; j++)
-            dst[RIDX(i, j, dim)] = avg(dim, i, j, src);
+  for (i = 0; i < dim; i++)
+    for (j = 0; j < dim; j++)
+      dst[RIDX(i, j, dim)] = avg(dim, i, j, src);
 }
 
 /*
@@ -218,29 +245,27 @@ void naive_smooth(int dim, pixel *src, pixel *dst)
  */
 
 char smooth_descr[] = "smooth: Current working version";
-void smooth(int dim, pixel *src, pixel *dst)
-{
-    // 处理四个角
-    set_corner(0, src, dst, 1, dim, dim + 1);
-    set_corner(dim - 1, src, dst, dim - 2, dim + dim - 2, dim + dim - 1);
-    set_corner(RIDX(dim - 1, 0, dim), src, dst, RIDX(dim - 1, 1, dim), RIDX(dim - 2, 0, dim), RIDX(dim - 2, 1, dim));
-    set_corner(RIDX(dim - 1, dim - 1, dim), src, dst, RIDX(dim - 1, dim - 2, dim), RIDX(dim - 2, dim - 2, dim), RIDX(dim - 2, dim - 1, dim));
-    // 处理四个边
-    for (int j = 1; j <= dim - 2; j++)
-    {
-        set_top(dim, src, dst, j);
-        set_bottom(dim, src, dst, dim * dim - dim + j);
-        set_left(dim, src, dst, j * dim);
-        set_right(dim, src, dst, j * dim + dim - 1);
+void smooth(int dim, pixel *src, pixel *dst) {
+  // 处理四个角
+  set_corner(0, src, dst, 1, dim, dim + 1);
+  set_corner(dim - 1, src, dst, dim - 2, dim + dim - 2, dim + dim - 1);
+  set_corner(RIDX(dim - 1, 0, dim), src, dst, RIDX(dim - 1, 1, dim),
+             RIDX(dim - 2, 0, dim), RIDX(dim - 2, 1, dim));
+  set_corner(RIDX(dim - 1, dim - 1, dim), src, dst, RIDX(dim - 1, dim - 2, dim),
+             RIDX(dim - 2, dim - 2, dim), RIDX(dim - 2, dim - 1, dim));
+  // 处理四个边
+  for (int j = 1; j <= dim - 2; j++) {
+    set_top(dim, src, dst, j);
+    set_bottom(dim, src, dst, dim * dim - dim + j);
+    set_left(dim, src, dst, j * dim);
+    set_right(dim, src, dst, j * dim + dim - 1);
+  }
+  // 中间部分
+  for (int i = 1; i <= dim - 2; i++) {
+    for (int j = 1; j <= dim - 2; j++) {
+      set_in(dim, src, dst, i * dim + j);
     }
-    // 中间部分
-    for (int i = 1; i <= dim - 2; i++)
-    {
-        for (int j = 1; j <= dim - 2; j++)
-        {
-            set_in(dim, src, dst, i * dim + j);
-        }
-    }
+  }
 }
 
 /*********************************************************************
@@ -251,9 +276,8 @@ void smooth(int dim, pixel *src, pixel *dst)
  *     registered test function.
  *********************************************************************/
 
-void register_smooth_functions()
-{
-    add_smooth_function(&smooth, smooth_descr);
-    add_smooth_function(&naive_smooth, naive_smooth_descr);
-    /* ... Register additional test functions here */
+void register_smooth_functions() {
+  add_smooth_function(&smooth, smooth_descr);
+  add_smooth_function(&naive_smooth, naive_smooth_descr);
+  /* ... Register additional test functions here */
 }
